ceb3c09c731ed7fa3db735fdbb36ad8621b2a7d7
vefa <44091688+veefa@users.noreply.github.com>
2025-04-07 23:20:31 +0100

    Style HabitItem

--- src/components/HabitItem.tsx
+++ src/components/HabitItem.tsx
@@ -7,6 +7,7 @@ interface HabitItemProps {
     onComplete: (habitId: string) => void;
 }
  
+// Displays the habit name and frequency
 const HabitItem: React.FC<HabitItemProps> = ({habit, onComplete}) => {
     const handleCheckboxChange = () => {
         onComplete(habit.id)  // Mark as completed
@@ -14,14 +15,14 @@ const HabitItem: React.FC<HabitItemProps> = ({habit, onComplete}) => {
 
     return ( 
         <div
-        className="border rounded"
+        className="space-y-2 shadow p-4 border rounded-lg"
         style={{backgroundColor: habit.color}}
         >
-            <div className="flex items-center">
-                <h3>{habit.name}</h3>
+            <div className="flex justify-between items-center">
+                <h3 className="font-medium text-lg">{habit.name}</h3>
                 <input
                  type="checkbox" 
-                 checked={habit.history.includes(new Date().toISOString())}
+                 checked={habit.history.includes(new Date().toISOString())}  //to determine if the habit has already been completed today
                  onChange={handleCheckboxChange}
                  className="w-6 h-6"
                  />
